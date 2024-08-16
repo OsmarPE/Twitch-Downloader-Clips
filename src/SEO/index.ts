f2e8ec8b842ca20163559339c23900db745bbf6d
OsmarPE <osmarperera10@gmail.com>
2024-08-16 15:41:28 -0600

    fixed SEO URL

--- src/SEO/index.ts
+++ src/SEO/index.ts
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 
 export const meta: Metadata = {
-  metadataBase: new URL('https://acme.com'),
+  metadataBase: new URL('https://ostw.vercel.app/'),
   title: "Twitch Clips Downloader - OSTW",
   description:
     "OSTW is the easiest way to download Twitch clips. Simply paste the Twitch clip URL into the box and click 'Download Clip.' We’ll provide the video file for you to download.",
