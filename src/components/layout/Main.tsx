fcb03ebb0a59d243be69ae6ec93059869f946843
OsmarPE <osmarperera10@gmail.com>
2024-08-19 14:02:02 -0600

    fixed button generate in phone

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -15,7 +15,7 @@ export default function Main({children}:Props) {
                 <div className="text-center">
                     <Badge type="secundary">Twitch</Badge>
                     <h1 className="text-4xl lg:text-5xl mt-6 font-bold text-white">Twitch Clips Downloader</h1>
-                    <p className="text-grayMain leading-normal text-pretty max-w-2xl mt-5 mx-auto mb-10">OSTW is the easiest way to download Twitch clips. Simply paste the Twitch clip URL into the box and click 'Download Clip.' We’ll provide the video file for you to download.</p>
+                    <p className="text-grayMain leading-normal text-pretty max-w-2xl mt-5 mx-auto mb-10">OSTW is the easiest way to download Twitch clips. Simply paste the Twitch clip URL into the box and click 'Generate.' We’ll provide the video file for you to download.</p>
                     <Form/>
                     {children}
                 </div>
